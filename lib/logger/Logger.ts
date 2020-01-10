9fdc58ab231a484a52c4e1eaef6103856b3bc85c
Vitor Rangel <rangel.final@gmail.com>
2020-01-10 18:33:59 -0300

    Refactor: Add prod transport & use default winston formaters

--- lib/logger/Logger.ts
+++ lib/logger/Logger.ts
@@ -1,36 +1,70 @@
-import * as winston from 'winston';
-import * as Transport from 'winston-transport';
-import { BaseError } from '../BaseError';
-import { enumerateErrorFormat, lineFormat } from "../utils";
+import * as winston from "winston";
+import * as Transport from "winston-transport";
+import { BaseError } from "../BaseError";
+import { stringfyInfo } from "../utils";
 
 export interface LoggerOptions extends winston.LoggerOptions {
   transports?: Transport[];
 }
 
+export type notError<T> = T extends Error ? never : T;
+
+// Override the LeveledLogMethod to remove the (infoObject: object) overload as this allows calls that don't work, like log(new Error())
+interface LeveledLogMethod {
+  (message: string, callback: winston.LogCallback): Logger;
+  (message: string, meta: any, callback: winston.LogCallback): Logger;
+  (message: string, ...meta: any[]): Logger;
+  <T>(infoObject: notError<T>): Logger;
+}
+
+type logMethods = {
+  // for cli and npm levels
+  error: LeveledLogMethod;
+  warn: LeveledLogMethod;
+  help: LeveledLogMethod;
+  data: LeveledLogMethod;
+  info: LeveledLogMethod;
+  debug: LeveledLogMethod;
+  prompt: LeveledLogMethod;
+  http: LeveledLogMethod;
+  verbose: LeveledLogMethod;
+  input: LeveledLogMethod;
+  silly: LeveledLogMethod;
+
+  // for syslog levels only
+  emerg: LeveledLogMethod;
+  alert: LeveledLogMethod;
+  crit: LeveledLogMethod;
+  warning: LeveledLogMethod;
+  notice: LeveledLogMethod;
+};
+
 // Export the winston.Logger type so we don't need to install the winston types on dependants
-export type LoggerInstance = winston.Logger;
+export type LoggerInstance = Omit<winston.Logger, keyof logMethods> & logMethods;
 
 export class Logger {
   /**
    * The singleton logger instance, needs to be created using `Logger.initialize()`.
-   * 
+   *
    * @see Logger.initialize()
    */
   protected static instance: LoggerInstance;
 
-  /**
-   * The default transports thay will be enabled in the singleton.
-   */
-  static DEFAULT_TRANSPORTS: LoggerInstance['transports'] = [
-    new winston.transports.Console({
-      level: process.env.LOG_LEVEL || 'silly',
-      format: winston.format.combine(
-        enumerateErrorFormat(),
-        winston.format.colorize(),
-        lineFormat(),
-      ),
-    }),
-  ];
+  static DEFAULT_DEVELOPMENT_TRANSPORT = new winston.transports.Console({
+    level: process.env.LOG_LEVEL || "info",
+    format: winston.format.combine(winston.format.errors({ stack: true }), winston.format.printf(stringfyInfo))
+  });
+
+  static DEFAULT_PRODUCTION_TRANSPORT = new winston.transports.Console({
+    level: process.env.LOG_LEVEL || "silly",
+    format: winston.format.combine(
+      winston.format.errors({ stack: true }),
+      winston.format.timestamp(),
+      winston.format.json()
+    )
+  });
+
+  static DEFAULT_TRANSPORTS = [Logger.DEFAULT_DEVELOPMENT_TRANSPORT];
 
   /**
    * Simple logger constructor is deprecated, use SimpleLogger.initialize() instead.
@@ -38,7 +72,7 @@ export class Logger {
    * @deprecated
    */
   private constructor() {
-    throw new Error('Simple logger constructor is deprecated in Winston 3, use Logger.initialize() instead');
+    throw new Error("Simple logger constructor is deprecated in Winston 3, use Logger.initialize() instead");
   }
 
   /**
@@ -46,7 +80,7 @@ export class Logger {
    */
   public static getInstance(): LoggerInstance {
     if (!this.instance) {
-      throw new BaseError('Logger has not been initialized yet');
+      throw new BaseError("Logger has not been initialized yet");
     }
     return this.instance;
   }
@@ -59,14 +93,13 @@ export class Logger {
   public static initialize(options: LoggerOptions = {}): LoggerInstance {
     // Prepare default console transport
     const opt = {
-      transports: options.transports || Logger.DEFAULT_TRANSPORTS,
+      transports: options.transports || Logger.DEFAULT_TRANSPORTS
     };
 
-    // Construct new Winston logger instance with enhanced error handling
+    // Construct new Winston logger instance
     const logger = winston.createLogger({
-      format: winston.format.combine(enumerateErrorFormat()),
       ...opt
-    });;
+    });
 
     if (!this.instance) {
       this.instance = logger;
--- lib/utils/logger.utils.ts
+++ lib/utils/logger.utils.ts
@@ -1,92 +1,50 @@
-import * as Sentry from '@sentry/node';
-import jsonStringify from 'fast-safe-stringify';
-import * as os from 'os';
-import { MESSAGE } from 'triple-beam';
-import { format, info } from 'winston';
-import { BaseError } from '../BaseError';
-
-export const lineFormat = format((info: any) => {
-  const stringifiedRest = jsonStringify({
-    ...info,
-    level: undefined,
-    message: undefined,
-    splat: undefined
-  }, null, 2);
-
-  const padding = info.padding && info.padding[info.level] || '';
-  if (stringifiedRest !== '{}') {
-    info[MESSAGE] = `${info.level}:${padding} ${info.message} ${stringifiedRest}`;
-  } else {
-    info[MESSAGE] = `${info.level}:${padding} ${info.message}`;
-  }
-
-  return info;
-});
-
-// Quick and dirty fix for Winston@3.0.0 issue with errors
-// @see {https://github.com/winstonjs/winston/issues/1338}
-export const enumerateErrorFormat = format((info: any) => {
-  if (info.message instanceof BaseError) {
-    return {
-      message: info.message.message,
-      stack: info.message.stack,
-      ...info.message
-    };
-  }
-
-  if (info.message instanceof Error) {
-    return {
-      message: info.message.message,
-      stack: info.message.stack,
-      ...info.message
-    };
-  }
-
-  if (info instanceof BaseError) {
-    return {
-      message: info.message,
-      stack: info.stack,
-      ...info,
-    };
-  }
-
-  if (info instanceof Error) {
-    return {
-      message: info.message,
-      stack: info.stack,
-      ...info,
-    };
-  }
-
-  return info;
-});
-
+import * as Sentry from "@sentry/node";
+import * as os from "os";
+import { LEVEL, SPLAT, MESSAGE } from "triple-beam";
+import { inspect } from "util";
 
 export const winstonLevelToSentryLevel = {
-  silly: 'debug',
-  verbose: 'debug',
-  info: 'info',
-  debug: 'debug',
-  warn: 'warning',
-  error: 'error',
-  default: info,
+  silly: "debug",
+  verbose: "debug",
+  debug: "debug",
+  info: "info",
+  warn: "warning",
+  error: "error",
+  default: "info"
 };
 
+export const stringfyInfo = (info: any) => {
+  const depth = process.env.LOG_LEVEL_DEPTH ? Number(process.env.LOG_LEVEL_DEPTH) : 5;
 
-export const prepareSentryMeta = (info: { level: string, tags: any, message: any }): [Sentry.Event | Error, object] => {
-  const {
-    level,
-    tags,
-    modules,
-    platform = os.platform(),
-    server_name = os.hostname(),
-    ...extra
-  }: any = { ...info };
+  const extra = getExtraInfo(info, depth);
+  let message =
+    typeof info.message === "string"
+      ? info.message || info.originalMessage
+      : "\n" + inspect(info.message || info.originalMessage, { depth, colors: true });
+  if (extra) {
+    return `${info.level}: ${message} \n${extra}`;
+  }
+  return `${info.level}: ${message}`;
+};
+
+export const getExtraInfo = (info: any, depth: number) => {
+  const blacklist = [LEVEL, SPLAT, MESSAGE, "message", "level"];
+  const extra = {};
+  Object.getOwnPropertyNames(info).forEach(function(key) {
+    if (!blacklist.includes(key)) extra[key] = info[key];
+  });
+
+  if (Object.keys(extra).length === 0) return undefined;
+  return inspect(extra, { depth, colors: true });
+};
+
+export const prepareSentryMeta = (info: { level: string; tags: any; message: any }): [Sentry.Event | Error, object] => {
+  const { level, tags, modules, platform = os.platform(), server_name = os.hostname(), ...extra }: any = { ...info };
 
   let stack: string | undefined;
 
-  // Generate mocked stack for objects
-  if (info.level !== 'error') {
+  // Generate mocked stack for o`bjects
+  if (info.level !== "error") {
     const event = new Error(info.message);
     event.name = info.level;
     stack = event.stack;
@@ -99,16 +57,16 @@ export const prepareSentryMeta = (info: { level: string, tags: any, message: any
     tags: {
       platform,
       stackId: extra.stackId,
-      ...tags,
+      ...tags
     },
     message: info.message.message || info.message,
-    level: winstonLevelToSentryLevel[info.level],
+    level: winstonLevelToSentryLevel[info.level]
   };
 
   const extraObject = {
-      stack,
-      ...extra,
-  }
+    stack,
+    ...extra
+  };
 
   return [meta, extraObject];
 };
